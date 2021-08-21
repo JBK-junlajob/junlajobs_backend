d67b2409eb3a7ce3ee501653c7197d1921e1981e
EPJame <ekkachai.jame28@gmail.com>
2021-08-21 21:42:13 +0700

    fix path account

--- src/main/java/com/junlajobs_backend/controller/UserManagementController.java
+++ src/main/java/com/junlajobs_backend/controller/UserManagementController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.ExecutionException;
 
 @RestController
-@RequestMapping("/usermanagement")
+@RequestMapping("/account")
 public class UserManagementController {
 
     @Autowired
