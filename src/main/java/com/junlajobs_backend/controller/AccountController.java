6d1037abc7eaefb5c1f206d3e89df2576da598fe
EPJame <ekkachai.jame28@gmail.com>
2021-08-22 01:25:36 +0700

    add CRUD of User

---
+++ src/main/java/com/junlajobs_backend/controller/AccountController.java
@@ -0,0 +1,38 @@
+package com.junlajobs_backend.controller;
+
+import com.junlajobs_backend.model.UserEntity;
+import com.junlajobs_backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/saveuser")
+    public  String saveUser(@RequestBody UserEntity user) throws ExecutionException, InterruptedException {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/getuser/{username}")
+    public  UserEntity getUser(@PathVariable(value = "username")String username) throws ExecutionException, InterruptedException {
+        return userService.getUserAccount(username);
+    }
+
+    @PostMapping("/updateuser")
+    public  String updateUser(@RequestBody UserEntity user) throws ExecutionException, InterruptedException {
+        return userService.updateUser(user);
+    }
+
+    @GetMapping("/deleteuser/{username}")
+    public  String deleteUser(@PathVariable(value = "username")String username) throws ExecutionException, InterruptedException {
+        return userService.deleteUser(username);
+    }
+
+
+}
--- src/main/java/com/junlajobs_backend/service/UserService.java
+++ src/main/java/com/junlajobs_backend/service/UserService.java
@@ -1,6 +1,8 @@
 package com.junlajobs_backend.service;
 
 import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
@@ -22,5 +24,41 @@ public class UserService {
 
         return colApiFuture.get().getUpdateTime().toString();
     }
+
+    public String deleteUser(String username) throws ExecutionException, InterruptedException {
+        Firestore dbFireStore = FirestoreClient.getFirestore();
+
+        ApiFuture<WriteResult> colApiFuture = dbFireStore.collection(COLLECTION_USER).document(username).delete();
+
+        return colApiFuture.get().getUpdateTime().toString();
+    }
+
+    public String updateUser(UserEntity user) throws ExecutionException, InterruptedException {
+        Firestore dbFireStore = FirestoreClient.getFirestore();
+
+        ApiFuture<WriteResult> colApiFuture = dbFireStore.collection(COLLECTION_USER).document(user.getUsername()).set(user);
+
+        return colApiFuture.get().getUpdateTime().toString();
+    }
+
+    public UserEntity getUserAccount(String username) throws ExecutionException, InterruptedException {
+        Firestore dbFireStore = FirestoreClient.getFirestore();
+
+        DocumentReference documentReference = dbFireStore.collection(COLLECTION_USER).document(username);
+
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+
+        DocumentSnapshot document=future.get();
+
+        UserEntity userEntity = null;
+        if(document.exists()){
+            userEntity = document.toObject(UserEntity.class);
+            return userEntity;
+        }else{
+            return null;
+        }
+    }
+
+
 }
 
